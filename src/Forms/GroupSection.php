0cda108929371ff3420896ffda16046938ea8414
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-08-09 11:23:27 -0500

    Create GroupSection

---
+++ src/Forms/GroupSection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace EncoreDigitalGroup\FilamentHelpers\Forms;
+
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+
+/** @experimental */
+class GroupSection
+{
+    public static function make(array $schema, int $outerColumns = 1, int $innerColumns = 1): Component
+    {
+        return Group::make()
+            ->schema([
+                Section::make()
+                    ->schema($schema)
+                    ->columns($innerColumns)
+            ])
+            ->columns($outerColumns);
+    }
+}
