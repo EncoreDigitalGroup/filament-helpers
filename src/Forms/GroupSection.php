7d891ed3ab30746f67773f66553324e9ec59d4d2
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-08-17 20:25:47 -0500

    [Forms] GroupSection Column Span Support (#13)

--- src/Forms/GroupSection.php
+++ src/Forms/GroupSection.php
@@ -9,14 +9,28 @@ use Filament\Forms\Components\Section;
 /** @experimental */
 class GroupSection
 {
-    public static function make(array $schema, int $outerColumns = 2, int $innerColumns = 2): Component
+    public static function make(array $schema, int $outerColumns = 2, int $innerColumns = 1, int $outerColumnSpan = 2, int $innerColumnSpan = 1): Component
     {
+        if ($innerColumns > $outerColumns) {
+            $innerColumns = $outerColumns;
+        }
+
+        if ($outerColumnSpan > $outerColumns) {
+            $outerColumnSpan = $outerColumns;
+        }
+
+        if ($innerColumnSpan > $innerColumns) {
+            $innerColumnSpan = $innerColumns;
+        }
+
         return Group::make()
             ->schema([
                 Section::make()
                     ->schema($schema)
-                    ->columns($innerColumns),
+                    ->columns($innerColumns)
+                    ->columnSpan($innerColumnSpan),
             ])
-            ->columns($outerColumns);
+            ->columns($outerColumns)
+            ->columnSpan($outerColumnSpan);
     }
 }
