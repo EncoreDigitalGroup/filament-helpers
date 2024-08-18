7f8feb32c4e2682be40017ea66d6e2be1eaf0990
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-08-17 20:31:10 -0500

    [Forms] Restore Defaults (#14)

--- src/Forms/GroupSection.php
+++ src/Forms/GroupSection.php
@@ -9,7 +9,7 @@ use Filament\Forms\Components\Section;
 /** @experimental */
 class GroupSection
 {
-    public static function make(array $schema, int $outerColumns = 2, int $innerColumns = 1, int $outerColumnSpan = 2, int $innerColumnSpan = 1): Component
+    public static function make(array $schema, int $outerColumns = 2, int $innerColumns = 2, int $outerColumnSpan = 2, int $innerColumnSpan = 2): Component
     {
         if ($innerColumns > $outerColumns) {
             $innerColumns = $outerColumns;
