aee3ebda270ff58cf378d8c7cfbcfcca39a57b9c
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-11-12 20:46:11 -0600

    [Select] Add Base Select (#19)

---
+++ src/InputTypes/Select/Select.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Copyright (c) 2023-2024. Encore Digital Group.
+ * All Rights Reserved.
+ */
+
+namespace EncoreDigitalGroup\Filament\Helpers\InputTypes\Select;
+
+use Closure;
+use Filament\Forms\Components\Select as BaseSelect;
+use Illuminate\Contracts\Support\Htmlable;
+
+class Select
+{
+    public static function make(string $fieldName, string|Htmlable|Closure|null $label): BaseSelect
+    {
+        return BaseSelect::make($fieldName)
+            ->label($label)
+            ->native(false);
+    }
+}
