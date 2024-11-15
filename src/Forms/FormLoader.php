5fda2b4f01e00f9e872fe723c35f28c3ac03fd2b
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-11-14 19:23:01 -0600

    [Forms] Create FormLoader (#20)
    Co-authored-by: onairmarc <onairmarc@users.noreply.github.com>

---
+++ src/Forms/FormLoader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EncoreDigitalGroup\Filament\Helpers\Forms;
+
+use Filament\Forms\Form;
+use Filament\Resources\Pages\Page;
+use Illuminate\Database\Eloquent\Model;
+
+class FormLoader
+{
+    protected Page $context;
+    protected Model $model;
+    protected string $method;
+    protected string $statePath = 'data';
+
+    public static function make(Page $context, Model $model, ?string $method, ?string $statePath = 'data'): array
+    {
+        return [
+            'form' => $context->form($context::getResource()::$method(
+                Form::make($context)
+                    ->operation('edit')
+                    ->model($model)
+                    ->statePath($statePath)
+                    ->columns($context->hasInlineLabels() ? 1 : 2)
+                    ->inlineLabel($context->hasInlineLabels()),
+            )),
+        ];
+    }
+}
