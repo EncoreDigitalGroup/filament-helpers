e1f91764a075353faf96ff15a18716ece8b262b6
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-01-04 15:32:18 -0600

    [Support] Create NavigationGroups (#22)
    Co-authored-by: onairmarc <onairmarc@users.noreply.github.com>

---
+++ src/Support/NavigationGroups.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EncoreDigitalGroup\Filament\Helpers\Support;
+
+enum NavigationGroups: string
+{
+    case Calendar = 'Calendar';
+    case ContentManagement = 'Content Management';
+    case CRM = 'CRM';
+    case Media = 'Media';
+    case People = 'People';
+    case Service = 'Service';
+    case Settings = 'Settings';
+    case Tenants = 'Tenant Management';
+}
