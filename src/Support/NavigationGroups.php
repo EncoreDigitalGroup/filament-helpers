04453131ced5008b17eb94fef6e4ef87cdc88459
EncoreBot <ghbot@encoredigitalgroup.com>
2025-02-09 01:01:27 +0000

    Rectifying

--- src/Support/NavigationGroups.php
+++ src/Support/NavigationGroups.php
@@ -4,12 +4,12 @@ namespace EncoreDigitalGroup\Filament\Helpers\Support;
 
 enum NavigationGroups: string
 {
-    case Calendar = 'Calendar';
-    case ContentManagement = 'Content Management';
-    case CRM = 'CRM';
-    case Media = 'Media';
-    case People = 'People';
-    case Service = 'Service';
-    case Settings = 'Settings';
-    case Tenants = 'Tenant Management';
+    case Calendar = "Calendar";
+    case ContentManagement = "Content Management";
+    case CRM = "CRM";
+    case Media = "Media";
+    case People = "People";
+    case Service = "Service";
+    case Settings = "Settings";
+    case Tenants = "Tenant Management";
 }
