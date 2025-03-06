d1f0b35e69e7c3dd817e21eecd6bb0456f27a77b
EncoreBot <ghbot@encoredigitalgroup.com>
2025-03-06 22:56:36 +0000

    Rectifying

--- src/InputTypes/Text/MoneyInput.php
+++ src/InputTypes/Text/MoneyInput.php
@@ -13,7 +13,7 @@ class MoneyInput
             ->numeric()
             ->prefix("$")
             ->step(0.01)
-            ->formatStateUsing(fn($state) => $state ? number_format($state / 100, 2, '.', '') : "0.00")
-            ->dehydrateStateUsing(fn($state) => $state ? $state * 100 : 0);
+            ->formatStateUsing(fn($state): string => $state ? number_format($state / 100, 2, ".", "") : "0.00")
+            ->dehydrateStateUsing(fn($state): int|float => $state ? $state * 100 : 0);
     }
 }
