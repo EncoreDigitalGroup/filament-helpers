d7d0966cb93347d33ccddeed88ed30b8c0da3a86
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-03-22 08:26:33 -0500

    Add Fluent Support to GroupSection (#28)
    Co-authored-by: EncoreBot <ghbot@encoredigitalgroup.com>

--- src/Forms/GroupSection.php
+++ src/Forms/GroupSection.php
@@ -9,28 +9,152 @@ use Filament\Forms\Components\Section;
 /** @experimental */
 class GroupSection
 {
-    public static function make(array $schema, int $outerColumns = 2, int $innerColumns = 2, int $outerColumnSpan = 2, int $innerColumnSpan = 2): Component
+    private array $schema = [];
+    private int $outerColumns = 2;
+    private int $innerColumns = 2;
+    private int $outerColumnSpan = 2;
+    private int $innerColumnSpan = 2;
+    private bool $collapsable = false;
+    private bool $defaultCollapsed = false;
+    private string $sectionHeading = "";
+
+    private function __construct(array $schema)
     {
+        $this->schema($schema);
+    }
+
+    public static function make(array $schema, int $outerColumns = 2, int $innerColumns = 2, int $outerColumnSpan = 2, int $innerColumnSpan = 2, bool $fluent = false): self|Component
+    {
+        $groupSection = new self($schema);
+
         if ($innerColumns > $outerColumns) {
-            $innerColumns = $outerColumns;
+            $groupSection->innerColumns($outerColumns);
         }
 
         if ($outerColumnSpan > $outerColumns) {
-            $outerColumnSpan = $outerColumns;
+            $groupSection->outerColumnSpan($outerColumns);
         }
 
         if ($innerColumnSpan > $innerColumns) {
-            $innerColumnSpan = $innerColumns;
+            $groupSection->innerColumnSpan($innerColumns);
         }
 
+        if ($fluent) {
+            return $groupSection;
+        }
+
+        return $groupSection->render();
+    }
+
+    public function schema(array $schema): self
+    {
+        $this->schema = $schema;
+
+        return $this;
+    }
+
+    public function getSchema(): array
+    {
+        return $this->schema;
+    }
+
+    public function getOuterColumns(): int
+    {
+        return $this->outerColumns;
+    }
+
+    public function outerColumns(int $outerColumns): self
+    {
+        $this->outerColumns = $outerColumns;
+
+        return $this;
+    }
+
+    public function getInnerColumns(): int
+    {
+        return $this->innerColumns;
+    }
+
+    public function innerColumns(int $innerColumns): self
+    {
+        $this->innerColumns = $innerColumns;
+
+        return $this;
+    }
+
+    public function getOuterColumnSpan(): int
+    {
+        return $this->outerColumnSpan;
+    }
+
+    public function outerColumnSpan(int $outerColumnSpan): self
+    {
+        $this->outerColumnSpan = $outerColumnSpan;
+
+        return $this;
+    }
+
+    public function getInnerColumnSpan(): int
+    {
+        return $this->innerColumnSpan;
+    }
+
+    public function innerColumnSpan(int $innerColumnSpan): self
+    {
+        $this->innerColumnSpan = $innerColumnSpan;
+
+        return $this;
+    }
+
+    public function collapsible(bool $collapsible = true): self
+    {
+        $this->collapsable = $collapsible;
+
+        return $this;
+    }
+
+    public function getCollapsible(): bool
+    {
+        return $this->collapsable;
+    }
+
+    public function sectionHeading(string $sectionHeading = ""): self
+    {
+        $this->sectionHeading = $sectionHeading;
+
+        return $this;
+    }
+
+    public function getSectionHeading(): string
+    {
+        return $this->sectionHeading;
+    }
+
+    public function defaultCollapsed(bool $collapsed = true): self
+    {
+        $this->defaultCollapsed = $collapsed;
+
+        return $this;
+    }
+
+    public function getDefaultCollapsed(): bool
+    {
+        return $this->defaultCollapsed;
+    }
+
+    public function render(): Component
+    {
         return Group::make()
             ->schema([
                 Section::make()
-                    ->schema($schema)
-                    ->columns($innerColumns)
-                    ->columnSpan($innerColumnSpan),
+                    ->schema($this->getSchema())
+                    ->columns($this->getInnerColumns())
+                    ->columnSpan($this->getInnerColumnSpan())
+                    ->collapsible($this->getCollapsible())
+                    ->collapsed($this->getDefaultCollapsed())
+                    ->heading($this->getSectionHeading()),
             ])
-            ->columns($outerColumns)
-            ->columnSpan($outerColumnSpan);
+            ->columns($this->getOuterColumns())
+            ->columnSpan($this->getOuterColumnSpan());
     }
 }
