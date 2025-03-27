c98ac82bc627144025f9a76af506a60f8b065b7f
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-03-27 15:57:44 -0500

    Add Navigation Groups and Icons (#30)

--- src/Support/NavigationIcon.php
+++ src/Support/NavigationIcon.php
@@ -4,12 +4,15 @@ namespace EncoreDigitalGroup\Filament\Helpers\Support;
 
 enum NavigationIcon: string
 {
+    case CreditCard = self::PREFIX . "credit-card";
+    case Dollar = self::PREFIX . "currency-dollar";
     case Stack = self::PREFIX . "rectangle-stack";
     case Home = self::PREFIX . "heroicon-o-home";
     case HomeModern = self::PREFIX . "home-modern";
     case Library = self::PREFIX . "building-library";
     case Office = self::PREFIX . "building-office";
     case Storefront = self::PREFIX . "building-storefront";
+    case User = self::PREFIX . "user";
 
     private const string PREFIX = "heroicon-o-";
 }
