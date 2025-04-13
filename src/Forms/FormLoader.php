395130f29c4be78bf6f97dc30eccb43a68d2b5d9
Marc Beinder <marc@marcbeinder.com>
2025-04-13 07:59:45 -0500

    Remove Unused Protected Class Properties

--- src/Forms/FormLoader.php
+++ src/Forms/FormLoader.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Rights Reserved.
+ */
 
 namespace EncoreDigitalGroup\Filament\Helpers\Forms;
 
@@ -8,11 +12,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormLoader
 {
-    protected Page $context;
-    protected Model $model;
-    protected string $method;
-    protected string $statePath = "data";
-
     public static function make(Page $context, Model $model, ?string $method, ?string $statePath = "data"): array
     {
         return [
